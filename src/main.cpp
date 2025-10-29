ed1f3492984ad3f3d9c61cccc556b86537aca917
ivanaryasatya <ivanas.thestar@gmail.com>
2025-10-29 08:36:55 +0700

    tambah custom lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,14 @@
-v#include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
+#include "RadioCom.h"
 
-RF24 radio(9, 10); // CE, CSN
-const byte address[6] = "00001"; // Alamat komunikasi
+RadioCom radio(9, 10, "00001", true); // CE, CSN, alamat, transmitter
 
 void setup() {
   Serial.begin(9600);
   radio.begin();
-  radio.openWritingPipe(address); // Atur alamat tujuan
-  radio.stopListening(); // Mode kirim
-  Serial.println("Transmitter siap!");
 }
 
 void loop() {
-  const char text[] = "Halo dari Transmitter!";
-  radio.write(&text, sizeof(text));
-  Serial.println("Pesan dikirim: Halo dari Transmitter!");
+  const char msg[] = "Halo Ivan!";
+  radio.send(msg, sizeof(msg));
   delay(1000);
 }
